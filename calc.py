75c9efda3328d8ac9732e791fcd60b30e3508c13
anamika03 <rashiikasingh191@gmail.com>
2023-04-17 22:57:52 +0530

    new program added

---
+++ calc.py
@@ -0,0 +1,11 @@
+print(2+3)  #add
+print(4-1)  #sub
+print(3*5)  #multiply
+print(7/3)  #float division
+print(7//3) #int division
+print(4%3)  #remainder
+print(2**3) #power 2 ka power 3 = 8
+print(round(2**0.5, 4))  #roundoff the floating digit till 4
+print(2**3/2*6-4*(3-4/2))
+#Precedence Rule
+# paranthesie --- exponent ---*,/,//,%,+,-
--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+#Print function:
+print("Hello World!")
+print('Hello World!')
+
+#Escape Sequences
+print("I\'m Anamika")
+# \n - next line
+# \t - tab
+# \b - backspace
+# \' - single qoute
+# \" - double qoute
+# \\ = backslash
+# ctrl +/ = to comment out the line
