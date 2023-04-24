27846165ac09b40a2be2f9b82eba4ddf608a44bb
anamika03 <rashiikasingh191@gmail.com>
2023-04-24 22:52:00 +0530

    code updated

--- string_methods.py
+++ string_methods.py
@@ -1,17 +1,34 @@
-name = "anamika Singh"
+# name = "anamika Singh"
 
-# len() function
-length = len(name)  #it will also count the space
-print(length)
+# # len() function
+# length = len(name)  #it will also count the space
+# print(length)
 
-# lower() method
-print(name.lower())
+# # lower() method
+# print(name.lower())
 
-# upper() method
-print(name.upper())
+# # upper() method
+# print(name.upper())
 
-# title() function
-print(name.title())
+# # title() function
+# print(name.title())
 
-# count() method
-print(name.count("i"))
+# # count() method
+# print(name.count("i"))
+
+name2 = "    An  am ika     "
+# lstrip() method - discard the left spaces
+print(name2.lstrip())
+
+# rstrip() method - discard the right spaces
+print(name2.rstrip())
+
+# strip() method - discard the all spaces before and after the string
+print(name2.strip())
+
+# replace(" ", "") - replacing the space with no space
+name_without_spaces = name2.replace(" ","")
+print(name_without_spaces)
+
+# find()
+print(name_without_spaces.find("a",3))
